d3a7c31da0b5e889d5a4bdd62783f3fced9cd565
bibhuti0001 <bibhuti0001@gmail.com>
2022-08-21 02:00:17 +0530

    modified

--- modules/security-group/variables.tf
+++ modules/security-group/variables.tf
@@ -2,7 +2,7 @@
 variable "secgroupname" {
   default = "my-sec-grp-for-tf2"
 }
-//Give your default AWS vpc id
+//Give your default AWS vpc id OR we can put our customize vpc id(after created it from UI)
 variable "vpc" {
    default = "vpc-0dc1f6b1736ddd7f5"
 }
